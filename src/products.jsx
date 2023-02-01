f39db5f1be2887d41cdeef6a07a608b366369cd4
shariq <shariq@g.c>
2023-02-01 10:18:22 +0530

    products change

--- src/products.jsx
+++ src/products.jsx
@@ -16,16 +16,17 @@ const Products = () => {
     }
     return (
         <div className="products mt-4">
-            <h1>Products Page</h1>
-            <div className="product">
+            <h1 className="text-center">Products Page</h1>
+            <hr />
+            <div className="product mt-5">
                 {productsData.map(data => (
                     <div className="d-flex border my-3">
                         <div className="img">
-                            <img style={{ cursor: "pointer" }} onClick={() => viewProduct(data.id)} width="300px" src={data.images[0]} alt="" />
+                            <img style={{ cursor: "pointer" }} onClick={() => viewProduct(data.id)} width="300px" src={data.thumbnail} alt="" />
                         </div>
                         <div className="p-5">
                             <h3 style={{ cursor: "pointer" }} onClick={() => viewProduct(data.id)}>{data.brand} {data.title}</h3>
-                            <p>{data.rating} ⭐</p>
+                            <p>rating : {data.rating} ⭐</p>
                             <p className="text-danger">-{data.discountPercentage}% <span className="text-dark fs-3 px-3"> ₹ {Math.round(data.price - data.price * (data.discountPercentage) / 100) * 80}</span></p>
                             <p className="text-secondary fs-6">MRP : <span className="text-secondary text-decoration-line-through fs-6">{data.price * 80}</span></p>
                         </div>
